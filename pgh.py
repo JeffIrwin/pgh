fc5e403deb63e80b2797a261bc20a4fefc6ad1c2
Jeff Irwin <jirwin505@gmail.com>
2020-04-23 20:47:40 -0400

    Handle errors; clone and set upstream first time if needed

--- pgh.py
+++ pgh.py
@@ -2,13 +2,8 @@
 # TODO:
 #
 #   - Error handling
-#       * at least "fatal: 'upstream' does not appear to be a git repository"
 #       * merge conflict?
-#   - Success/fail count at end of log
-#   - Add JSON entry for upstream URL, optionally set upstream first time
-#       * 'git remote add upstream https://github.com/ORG/REPO'
-#       * could even clone optionally.  save these in a separate all.json file
-#         or find a way to clone all repos in an org.
+#   - Add an option to clone all repos in an organization
 #   - Readme
 #
 
@@ -26,10 +21,15 @@ def printf(string):
 
 def sync_branch(branch):
     printf(this + "branch = " + branch)
-    os.system("git checkout " + branch)
-    os.system("git merge upstream/" + branch)
-    os.system("git push --all")
-    os.system("git checkout -")  # Go back to the last branch
+    io = os.system("git checkout " + branch)
+    #printf("io 1 = " + str(io))
+    if (io == 0): io = os.system("git merge upstream/" + branch)
+    #printf("io 2 = " + str(io))
+    if (io == 0): io = os.system("git push --all")
+    #printf("io 3 = " + str(io))
+    if (io == 0): io = os.system("git checkout -")  # Go back to the last branch
+    #printf("io 4 = " + str(io))
+    return io
 
 def sync_forks(args):
 
@@ -52,27 +52,81 @@ def sync_forks(args):
     path = pathlib.Path(args.forks).parent.absolute()
     printf(this + "path = " + str(path))
 
+    ntotal = 0
+    nfail  = 0
+    nsuccess = 0
+    failures = []
+
     for repo in repos:
         printf(this + "folder = " + repo["folder"])
-        os.chdir(path / repo["folder"])
+
+        io = 0
+        if (os.path.exists(path / repo["folder"])):
+            os.chdir(path / repo["folder"])
+        else:
+            if ("origin" in repo):
+                # If the folder doesn't exist, try cloning it from origin
+                origin = repo["origin"]
+                printf(this + "cloning from origin: " + origin)
+
+                if (not os.path.exists(path / repo["folder"] / "..")):
+                    os.mkdir(path / repo["folder"] / "..")
+
+                os.chdir(path / repo["folder"] / "..")
+                io = os.system("git clone --recursive " + origin)
+                if (io == 0): os.chdir(path / repo["folder"])
+
+            else:
+                printf(this + "error: folder \"" + str(path / repo["folder"]) + "\" does not exist and no origin is listed")
+                io = -1
 
         # Stash any unsaved changes.  It's a bad idea to run this script on
         # folders that you work out of, but we'll try to save just in case.
-        os.system("git stash")
+        stashing = False
+        if (io == 0):
+            stashing = True
+            io = os.system("git stash")
 
-        os.system("git fetch upstream")
+        if (io == 0):
+            io = os.system("git fetch upstream")
+            if (io != 0):
+                if ("upstream" in repo):
+                    # If upstream remote doesn't exist, try adding it.
+                    upstream = repo["upstream"]
+                    printf(this + "adding upstream remote: " + upstream)
+                    io = os.system("git remote add upstream " + upstream)
+                    io = os.system("git fetch upstream")
 
-        if "branches" in repo:
-            for branch in repo["branches"]:
-                sync_branch(branch)
-        else:
-            sync_branch(default_branch)
+                else:
+                    printf(this + "error: no upstream remote found for \"" + str(path / repo["folder"]) + "\"")
+                    io = -1
+
+        if (io == 0):
+            if "branches" in repo:
+                for branch in repo["branches"]:
+                    io = sync_branch(branch)
+                    ntotal += 1
+                    if (io == 0):
+                        nsuccess += 1
+            else:
+                io = sync_branch(default_branch)
+                ntotal += 1
+                if (io == 0):
+                    nsuccess += 1
+
+        if (io != 0):
+            nfail += 1
+            failures.append(repo["folder"])
 
         # Restore stashed changes to last branch
-        os.system("git stash pop")
+        if (stashing): os.system("git stash pop")
 
         printf('')
 
+    printf(this + "============ sync:  " + str(nsuccess) + " branches succeeded, " + str(nfail) + " failed ============")
+
+    if (nfail != 0):
+        printf(this + "failed in folders: " + str(failures))
     printf(this + "done")
 
 def pgh_parse_args():
